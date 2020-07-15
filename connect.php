22b6c47741b10c1add67186fdfaecc952c8b583f
PRATYAKSH GUPTA <pratyaksh1615@gmail.com>
2020-07-16 00:57:09 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,12 +1,8 @@
 <?php
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$countryCode = $_POST['countryCode'];
-$phone = $_POST['phone'];
+$date = $_POST['date'];
+$timeZoneId = $_POST['timeZoneId'];
+$slot = $_POST['slot'];
 $email = $_POST['email'];
-$gender = $_POST['gender'];
-$Languages = $_POST['Languages'];
-$msg = $_POST['msg'];
 
 
 
@@ -16,11 +12,11 @@ $msg = $_POST['msg'];
 
 
 
-if (!empty($fname) || !empty($lname) || !empty($countryCode) || !empty($phone) || !empty($email) || !empty($gender) || !empty($Languages) || !empty($msg))
+if (!empty($date) || !empty($timeZoneId) || !empty($slot) || !empty($email))
 { $host = 'healthmatch-server.mysql.database.azure.com';
   $username = 'HEALTHMATCH@healthmatch-server';
   $password = 'Hackathon2020';
-  $db_name = 'volunteer';
+  $db_name = 'schedule';
   
   //Establishes the connection
   $conn = mysqli_init();
@@ -29,30 +25,19 @@ if (!empty($fname) || !empty($lname) || !empty($countryCode) || !empty($phone) |
   die('Failed to connect to MySQL: '.mysqli_connect_error());
   }
   else {
-    $SELECT = "SELECT email From resister Where email =? Limit 1 ";
-    $INSERT = "INSERT Into resister (fname,lname,countryCode,phone,email,gender,Languages,msg) values(?, ?, ?, ?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($SELECT);
-    $stmt->bind_param("s",$email);
-    $stmt->execute();
-    $stmt->bind_result($email);
-    $stmt->store_result();
-    $rnum= $stmt->num_rows;
-    if($rnum==0){$stmt->close();
+    $INSERT = "INSERT Into bookings (date,timeZoneId,slot,email) values(?, ?, ?, ?)";
+
     $stmt = $conn->prepare($INSERT);
-    $b=implode(",",$Languages);
-    $stmt->bind_param("ssiissss",$fname,$lname,$countryCode,$phone,$email,$gender,$b,$msg);
+    $b=implode(",",$slot);
+    $stmt->bind_param("ssss",$date,$timeZoneId,$slot,$email);
     $stmt->execute();
-    header("location: thankyouvolunteer.php");
+    echo "slot booked";             
+    header("location: submitted.php");
     exit;
   }
-  else{ echo"there exists";}
   $stmt->close();
   $conn->close();
 }
 }
 
-  else{
-    echo "all fields required";
-    die();
-  }
   ?>

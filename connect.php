b4abfb5e56f5047df22589b70c78c380a6b17ead
PRATYAKSH GUPTA <pratyaksh1615@gmail.com>
2020-07-15 23:58:36 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,30 +5,26 @@
      $email = $_POST['email'];
 
 
-  $host = 'healthmatch-server.mysql.database.azure.com';
-  $username = 'HEALTHMATCH@healthmatch-server';
-  $password = 'Hackathon2020';
-  $db_name = 'schedule';
-  
-  //Establishes the connection
-  $conn = mysqli_init();
-  mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-  if (mysqli_connect_errno($conn)) {
-  die('Failed to connect to MySQL: '.mysqli_connect_error());
-  }
+     $conn = new mysqli('localhost','root','root','schedule');
+     if($conn->connect_error)
+     {
+       die('connection failed :' .$conn->connect_error);
+
+     }
      else
 
      {
-       $INSERT = "insert into bookings(date, timeZoneId, slot, email) values(?, ?, ?, ?)";   
-       $stmt = $conn->prepare($INSERT);
-       $b=implode(",",$slot);
+       $stmt = $conn->prepare("insert into bookings(date, timeZoneId, slot, email)
+       values(?, ?, ?, ?)");
+      $b=implode(",",$slot);
        $stmt->bind_param("ssss",$date,$timeZoneId, $b, $email);
        $stmt->execute();
        echo "slot booked";
-       header("Location:submitted.php");
+      header("Location:submitted.php");
        $stmt->close();
        $conn->close();
 
      }
 
  ?>
+

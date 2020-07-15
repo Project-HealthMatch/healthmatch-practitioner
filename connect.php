cf99e32cc2b8fd9cbb9d6f5107fb54505c46d1ee
PRATYAKSH GUPTA <pratyaksh1615@gmail.com>
2020-07-15 23:36:14 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -19,8 +19,7 @@
      else
 
      {
-       $stmt = $conn->prepare("insert into bookings(date, timeZoneId, slot, email)
-       values(?, ?, ?, ?)");
+       $stmt = $conn->prepare("insert into bookings(date, timeZoneId, slot, email) values(?, ?, ?, ?)");
       $b=implode(",",$slot);
        $stmt->bind_param("ssss",$date,$timeZoneId, $b, $email);
        $stmt->execute();

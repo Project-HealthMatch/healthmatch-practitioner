a94c35228ca90fcf9e277589315ed5dd64fd4b89
Dwitrisha <59766408+Dwitrisha@users.noreply.github.com>
2020-08-04 14:13:42 +0530

    Update request.php

--- request.php
+++ request.php
@@ -1,318 +1,311 @@
-<?
-session_start();
-
-if($_SESSION["user"]==true){
-
-}
-else{
-    header("Location: index.php");
-}
-?>
-
-
-
-
-
 <!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <style>
+      @import url("https://fonts.googleapis.com/css?family=Montserrat:400,800");
+      * {
+       margin: 0;
+       padding: 0;
+      }
+      body {
+      background-color: #c3dae9;
 
-   <html>
-       <head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<style>
 
-.topnav {
-  background-color: rgb(142, 196, 221);
-  overflow: hidden;
-  margin-bottom: 1rem;
-}
+        font-family: "Montserrat", sans-serif;
 
-/* Style the links inside the navigation bar */
-.topnav a {
-  float: left;
-  color: #000000;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
+     height: 100%;
+    padding-right: 2%;
+    padding-left:2% ;
+      }
 
-/* Change the color of links on hover */
-.topnav a:hover {
-  background-color: #ddd;
+select {
+  background-color: rgba(247, 247, 247, 0.973);
+  width: 20%;
+  margin-right: 0;
+
+  border-radius: 5px 5px 5px 5px;
   color: black;
 }
 
-/* Add a color to the active/current link */
-.topnav a.active {
-  background-color: #032b5a;
-  color: white;
-}
-
-
-.head{
- height:20%;
- width:100%;
- background-color: #bc94dc;
- color: white;
- padding: 0.5rem;
- text-align: center;
- font-size: 1.5rem;
-}
-
-.grid{
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr;
-}
-.card {
-  width:650px;
-  margin-bottom: 1rem;
-  border-radius: 5px;
-}
-
-form{
-  padding-left: 2rem;
-  padding-right: 2rem;
-  width:99%;
-  font-weight:600;
-}
-
-label{
-  font-size: 1.2rem;
-}
-
-input{
-  color: grey;
-  background-color: white;
-  font-size: 1rem;
-}
-
-.back{
-  width: 99%;
-  padding: 5rem;
-  height: 700px;
-  text-align:center;
-
-  background-image: linear-gradient( rgb(255, 255, 255) 10%, rgb(204, 190, 240)100%);
-
-  border-radius: 5px;
-}
-
-body {
-        background-image: linear-gradient(-225deg, #e3fdf5 0%, #ffe6fa 100%);
-        background-image: linear-gradient(to top, #00423f 0%, #0a0f49 100%);
-        background-attachment: fixed;
-        background-repeat: no-repeat;
-
-        font-family: "Vibur", cursive;
-        /*   the main font */
-        font-family: "Abel", sans-serif;
-        margin-bottom: 1rem;
-        /* background-image: linear-gradient(to top, #d9afd9 0%, #97d9e1 100%); */
+      h1 {
+        font-weight: bold;
+        margin: 0;
       }
-      .title{
-        color: white;
-        font-size: 2rem;
-        font-weight: 600;
+      h2 {
+        text-align: center;
       }
-      .cardtitle{
-        font-size: 1.3rem;
-
+      p {
+        font-size: 14px;
+        font-weight: 100;
+        line-height: 20px;
+        letter-spacing: 0.5px;
+        margin: 20px 0 30px;
       }
-      .viewbtn{
-        display: block;
-        width: 100px;
-        height: 35px;
-        border-radius: 25px;
-        outline: none;
-        border: none;
-        background-image: linear-gradient(to right, #45a29e, #5bcadd, #3292be);
+      span {
+        font-size: 12px;
+      }
+      a {
+        color: #333;
+        font-size: 14px;
+        text-decoration: none;
+        margin: 15px 0;
+      }
+      button {
+        border-radius: 20px;
+       background-image: linear-gradient(to right, #126e8a, #5bc3dd, #4bb8d3);
         background-size: 200%;
-        font-size: 1.2rem;
-        color: #fff;
-        font-family: "Poppins", sans-serif;
+        border: solid white 0.08rem;
+        color: #ffffff;
+        font-size: 12px;
+        font-weight: bold;
+        padding: 12px 45px;
+        letter-spacing: 1px;
         text-transform: uppercase;
-        margin: 1rem 0;
-        cursor: pointer;
-        transition: 0.5s;
+        transition: transform 80ms ease-in;
       }
-      .viewbtn:hover {
-        background-position: right;
+      button:active {
+        transform: scale(0.95);
       }
 
-      .acceptbtn{
-        display: block;
-        width: 150px;
-        height: 35px;
-        border-radius: 25px;
-        outline: none;
-        border: none;
-       background-image: linear-gradient(to right, #23bb6f, #5bddc1, #10994e);
-        background-size: 200%;
-        font-size: 1.2rem;
-        color: #fff;
-        font-family: "Poppins", sans-serif;
-        text-transform: uppercase;
-        margin: 1rem 0;
-        cursor: pointer;
-        transition: 0.5s;
-      }
-      .acceptbtn:hover {
+      button:hover {
         background-position: right;
       }
 
 
-      i{
-        margin-right:0.3rem;
+      input {
+       border-radius: 5px;
+        border: none;
+        padding: 12px 15px;
+        margin-top:20px ;
+        width: 90%;
+        background-color: rgba(247, 247, 247, 0.973);
+      }::placeholder{
+          color: black;
       }
 
+      input[type="tel"] {
+  width: 60%;
+  height: 50px;
+  background-color: rgba(247, 247, 247, 0.973);
 
-      @media(max-width:900px)
-      {
 
-        .topnav a {
- 
-  font-size: 1.2rem;
+
+  outline: none;
+  border: none;
+  margin-left: 0rem;
+  border-radius: 5px 5px 5px 5px;
+  transition: 0.2s linear;
 }
 
+
+
+
+.select {
+  background-color: rgba(247, 247, 247, 0.973);
+  width: 30%;
+
+  height: 50px;
+  border-radius: 5px 5px 5px 5px;
+  color: black;
+}
+
+
+      .logo{
+          width: 20%;
       }
 
-</style>
-<div class="topnav">
-        <div style="text-align:right;" >
+        .select{
+     height: 45px;
+    margin: 0;
+    }
 
-  <a href="<?php echo "dashboard.php"; ?>"><i class="fa fa-home"></i>Slot</a>
+    
 
-  <a href="<?php echo "logout.php"; ?>"><i class="fa fa-power-off"></i>Logout</a>
+      @media(max-width:900px){
 
+      .logo{
+          width: 40%;
+      margin:0;
+    
+      }
+    nav{
+      margin: 0;
+    }
+    li a{
+      margin: 0;
+    }
 
-  <a href="<?php echo "request.php"; ?>"><i class="fa fa-user"></i>Appoinments</a>
-             <a href="<?php echo "reset.php"; ?>"><i class="fa fa-tasks"></i>Change Password</a>
-     </div>
-     </div>
-       </head>
-
-       <div class="grid">
-<div style=" width:100%;">
-<div style="text-align:center;">
-<h5 class="title">Available Appointments</h5>
-</div>
-       <div class="col-sm-4 mb-5">
-<?php
-$conn = mysqli_connect("healthmatch-server.mysql.database.azure.com","HEALTHMATCH@healthmatch-server","Hackathon2020", "appointment");
-  $sql = "SELECT * FROM `booking`";
-$result = $conn->query($sql);
-
-
-{ while($row = $result->fetch_assoc()){
-
-  echo '<div class = "card ">';
-  echo '<div class = "head">';
-  echo 'PATIENT ID :' .$row['id'];
-  echo '</div>';
-  echo '<div class ="card-body">';
-  echo '<h5 class= "cardtitle"> DATE: '.$row['date'];
-  echo '</h5>';
-  echo '<h5 class = "cardtitle"> TIMEZONE:' .$row['timezoneid'];
-  echo '</h5>';
-  echo '<h5 class = "cardtitle">SLOT :' .$row['slot'];
-  echo '</h5>';
-  echo '<div class = "float-right">';
-   echo '<form action="" method ="POST">';
-   echo '<input type = "hidden" name = "id" value='.$row["id"].'>';
-    echo '<input type = "submit" class ="viewbtn" value = "View" name ="view">';
-
-   echo '</form>';
-  echo '</div>';
-  echo '</div>';
-  echo '</div>';
-
-}
-
-}
-?>
-</div>
-</div>
-<div>
-<?php
-if(isset($_REQUEST['view']))
-{ $sql = "SELECT * FROM booking WHERE id = {$_REQUEST['id']}";
-  $result = $conn->query($sql);
-  $row = $result->fetch_assoc();
-
-}
-
-
-  ?>
-
- <div style="text-align:center;">
-
- <h5 class="title">Appointment Details</h5>
-
- </div>
-<div class ="back">
-  <div class = "form">
-
-  <form action="connecti.php" method="POST" name="google-sheet">
-
-    <label for = "id"> PATIENT ID </label>
-    <input type="text" class="form-control" id="id"
-    name = "id" value = "<?php if (isset($row['id'])) echo $row['id'];?>" readonly>
-
-        <label for = "date"> APPOINTMENT DATE </label>
-        <input type="text" class="form-control" id="date"
-        name = "date" value = "<?php if (isset($row['date'])) echo $row['date'];?>" readonly>
-        <label for = "timezoneid"> TIMEZONE </label>
-        <input type="text" class="form-control" id="timezoneid"
-        name = "timezoneid" value = "<?php if (isset($row['timezoneid'])) echo $row['timezoneid'];?>"readonly>
-
-         <label for = "date"> SLOT </label>
-        <input type="text" class="form-control" id="slot"
-        name = "slot" value = "<?php if (isset($row['slot'])) echo $row['slot'];?>" readonly>
-        <label for = "optional"> PATIENT'S NOTE </label>
-        <input type="text" class="form-control" id="optional"
-        name = "optional" value = "<?php if (isset($row['optional'])) echo $row['optional'];?>"readonly>
-        <label for = "date"> PATIENT'S EMAIL </label>
-        <input type="email" class="form-control" id="email"
-        name = "email" value = "<?php if (isset($row['email'])) echo $row['email'];?>" readonly>
-        <label for = "email"> DOCTOR'S EMAIL </label>
-        <input type="email" class="form-control" id="email"
-        name = "docemail" required>
-
-
-    <div class = "float-right">
-  <?php echo '<input type = "submit" class ="acceptbtn" value = "Accept" " name ="submit" >'; ?>
-  </div>
-
-</form>
-
-
-  </div>
-</div>
-</div>
-
-
-</body>
-
-     <script>
-            const scriptURL = 'https://script.google.com/macros/s/AKfycbzf_IWvMFS9Nzv7z6CKFbfNTnAmOPNS1SSwYtcTX6O3zdWQkHXk/exec'
-            const form = document.forms['google-sheet']
-          
-            form.addEventListener('submit', e => {
-              e.preventDefault()
-              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-                .then(response => 
-             
- alert("Thank you for accepting!")
   
 
-                )
-                .catch(error => console.error('Error!', error.message))
-            })
-          </script>
+    .select{
+     height: 45px;
+    margin: 0;
+    }
 
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+   
+
+   
+      }
+
+
+
+
+ button{
+      margin-left: 30%;
+      margin-top: 1rem;
+    }
+
+
+
+
+
+
+.navgrid{
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr ;
+
+  padding-right: 20%;
+}
+
+@media(max-width:900px) {
+input{
+  width: 87%;
+  margin-left: 1rem;
+}
+button{
+  margin-top: 1rem;
+  margin-left: 30%;
+}
+}
+
+@media(min-width: 900px)
+{
+ nav{
+  margin-top: 1rem;
+
+}
+
+.navgrid{
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 1fr;
+padding-right:27% ;
+
+
+}
+}
+
+
+     
+
+      @media (min-width: 800px) {
+
+         form {
+        background: #111;
+        border: 1px solid #191919;
+        border-radius: 0.4em;
+        bottom: 0;
+        box-shadow: 0 5px 10px 5px rgba(0, 0, 0, 0.2);
+        height: 50%;
+        left: 0;
+        margin: auto;
+        overflow: hidden;
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: 80%;
+        padding: 3rem;
+        margin-top: 10rem;
+      }
+      
+        form {
+          background: #111;
+          border: 1px solid #191919;
+          border-radius: 0.4em;
+          bottom: 0;
+          box-shadow: 0 5px 10px 5px rgba(0, 0, 0, 0.2);
+          height: 300px;
+          left: 0;
+          margin: auto;
+          overflow: hidden;
+          position: absolute;
+          right: 0;
+          top: 0;
+          width: 400px;
+          padding: 3rem;
+          margin-top: 15rem;
+        }
+      }
+
+      form:after {
+        background: linear-gradient(
+          to right,
+          #111111,
+          #444444,
+          #b6b6b8,
+          #444444,
+          #2f2f2f,
+          #272727
+        );
+        content: "";
+        display: block;
+      }
+
+
+
+    </style>
+  </head>
+  <body>
+
+
+<div style="text-align: center;">
+    <img src="logo.png" class="logo">
+
+      <h1 style="color: rgb(31, 33, 34);">RESET PASSWORD</h1>
+      </div>
+<br>
+
+    <div class="container" id="container">
+    
+      <div class="form-container sign-up-container">
+         
+         <br>
+        <form action="reset.php" method="POST">
+           
+     
+            <input id="Email" name="Email" type="email" placeholder="Email" />
+            <input id="Password" name="Password" type="password" placeholder="Enter Old Password" />
+            <input id="newf" name="newf" type="newf" placeholder="Enter New Password" />
+            <input id=conf name="conf" type="conf" placeholder="Confirm New Password" />
+
+
+<br>
+           <button name="submit" >SUBMIT</button>
+
+        </form>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+      </div>
+    </div>
+  </body>
+
 </html>

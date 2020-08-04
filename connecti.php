2ffdb7ceb6d06cdab354165df2a131c83d06e14b
PRATYAKSH GUPTA <pratyaksh1615@gmail.com>
2020-08-05 01:00:32 +0530

    Update connecti.php

--- connecti.php
+++ connecti.php
@@ -339,8 +339,6 @@ $slt= implode(",",$emptyarray);
             })
           </script>
 
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js">
-          </script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-          </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </html>

73e9c0620dcff572d358bea7d7c13ff8929a0846
Dwitrisha <59766408+Dwitrisha@users.noreply.github.com>
2020-08-07 14:49:19 +0530

    Update search.php

--- search.php
+++ search.php
@@ -273,9 +273,9 @@ height: 100%;
              margin-bottom: 1rem;
               transition: 0.2s linear;
              color:black;
-              width:90%;
+              width:40%;
       font-size: 2rem;
-		      margin-left:15%;
+		      margin-left:30%;
             }
 		    
 	    }

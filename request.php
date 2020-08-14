f3dd8486ac7bf248fab03d627af2e2824f66b1da
Dwitrisha <59766408+Dwitrisha@users.noreply.github.com>
2020-08-14 16:36:37 +0530

    Update request.php

--- request.php
+++ request.php
@@ -20,6 +20,15 @@ else{
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	         <script
+    type="text/javascript"
+    src="https://cdn.weglot.com/weglot.min.js"
+  ></script>
+  <script>
+    Weglot.initialize({
+      api_key: "wg_6595b397394b082513ae5269c8992a3d7",
+    });
+  </script>
 <style>
 
 .topnav {
